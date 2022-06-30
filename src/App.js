37eb9bd14dbbfffaddf5c3ce6c30d56a87fabd30
zubaidr <web.expert1221@gmail.com>
2022-06-30 18:14:46 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [apiData, setAPIData] = useState([]);
+  const [dispData, setDispData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch('https://api2.binance.com/api/v3/ticker/24hr')
+    .then(x => (x.json()))
+    .then(res => {
+      console.log('Response -> ', res);
+    });
+  }, []);
+
+  const pageChanged = (newPage) => {
+    const lastVisitedPage = (newPage - 1) * 10;
+  };
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
